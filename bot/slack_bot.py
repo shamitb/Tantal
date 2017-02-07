7934b3d969574fde30b56a8ffbb4ea04b22930a8
Shamit Bagchi <shamit@gmail.com>
2017-02-07 22:03:56 +0100

    Update slack_bot.py

--- bot/slack_bot.py
+++ bot/slack_bot.py
@@ -56,8 +56,12 @@ class SlackBot(object):
                 self.clients.rtm.server.login_data['team']['name'],
                 self.clients.rtm.server.domain))
 
+
+            logger.debug('Before')
             msg_writer = Messenger(self.clients)
-            event_handler = RtmEventHandler(self.clients, msg_writer, self.trump_corpus)
+            event_handler = RtmEventHandler(self.clients, msg_writer)
+            logger.debug('After')
+
 
             while self.keep_running:
                 for event in self.clients.rtm.rtm_read():
