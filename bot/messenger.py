de2edc3dc1a1b07adea84905c5ea4b32a1ece178
Shamit Bagchi <shamit@gmail.com>
2017-02-08 16:32:56 +0100

    Update messenger.py

--- bot/messenger.py
+++ bot/messenger.py
@@ -18,9 +18,7 @@ class Messenger(object):
 
     def write_help_message(self, channel_id):
         bot_uid = self.clients.bot_user_id()
-        txt = '{}\n{}\n{}\n{}'.format(
-            "I'm your friendly Slack bot written in Python.  I'll *_respond_* to the following commands:",
-            "> `hi <@" + bot_uid + ">` - I'll respond with a randomized greeting mentioning your user. :wave:",
+        txt = '{}\n{}\n{}\n{}\n{}\n{}'.format(
             "> `<@" + bot_uid + "> classify [text]` - Classify the text into different topics",
             "> `<@" + bot_uid + "> entity [text]` - Extract entities such as Organization, Location, person etc from the text",
             "> `<@" + bot_uid + "> sentiment [text]` - Retrieve the sentiment from the text",
